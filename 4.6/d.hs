a476d91032e7c7cd8ae860b6b487f6a6932234bc
egregors <root@egregors.com>
2020-11-06 19:47:56 +1000

    d

--- 4.6/d.hs
+++ 4.6/d.hs
@@ -6,12 +6,15 @@ class MapLike m where
   insert :: Ord k => k -> v -> m k v -> m k v
   delete :: Ord k => k -> m k v -> m k v
   fromList :: Ord k => [(k, v)] -> m k v
+  fromList [] = empty
+  fromList ((k, v) : xs) = insert k v (fromList xs)
 
 newtype ArrowMap k v = ArrowMap {getArrowMap :: k -> Maybe v}
 
 instance MapLike ArrowMap where
-  empty = ArrowMap (\_ -> Nothing)
-  lookup = undefined
-  insert = undefined
-  delete = undefined
-  fromList ((k,v):xs) = insert k v (fromList xs)
+  empty = ArrowMap $ const Nothing
+
+  lookup k m = (getArrowMap m) k
+
+  insert k v m = ArrowMap (\x -> if x == k then Just v else lookup x m)
+  delete k m = ArrowMap (\x -> if x == k then Nothing else lookup x m)
