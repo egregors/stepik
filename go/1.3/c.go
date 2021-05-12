48e5c695355a2acc2603000bf926a242e82458b7
egregors <root@egregors.com>
2021-05-12 21:51:59 +0200

    с

---
+++ go/1.3/c.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	phrase := readString()
+
+	// 1. Разбейте фразу на слова, используя `strings.Fields()`
+	// 2. Возьмите первую букву каждого слова и приведите
+	//    ее к верхнему регистру через `unicode.ToUpper()`
+	// 3. Если слово начинается не с буквы, игнорируйте его
+	//    проверяйте через `unicode.IsLetter()`
+	// 4. Составьте слово из получившихся букв и запишите его
+	//    в переменную `abbr`
+	// ...
+	var abbr []rune
+	for _, w := range strings.Fields(phrase) {
+		var r rune
+		for _, ch := range w {
+			r = ch
+			break
+		}
+		if unicode.IsLetter(r) {
+			abbr = append(abbr, unicode.ToUpper(r))
+		}
+	}
+
+	fmt.Println(string(abbr))
+}
+
+// ┌─────────────────────────────────┐
+// │ не меняйте код ниже этой строки │
+// └─────────────────────────────────┘
+
+// readString читает строку из `os.Stdin` и возвращает ее
+func readString() string {
+	rdr := bufio.NewReader(os.Stdin)
+	str, _ := rdr.ReadString('\n')
+	return str
+}
